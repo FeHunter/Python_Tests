28493469af7ca1209ab82fd3570f6301adfebf9f
FeHunter <hunterfe1111@gmail.com>
2025-09-13 20:09:14 -0300

    feat: ball behavior and collisions

--- Tenis/tenis.py
+++ Tenis/tenis.py
@@ -4,6 +4,8 @@ import random
 SCREEN_WIDTH = 500
 SCREEN_HEIGHT = 500
 player1_y = 100
+player1_width = 20
+ball_size = 130
 ball_y = 200
 ball_x = 200
 ball_dx = random.choice([-5, 5])
@@ -22,15 +24,24 @@ def MoveDown(event):
     canvas.coords(player1, 0, player1_y, 20, player1_y + 100)
 
 def BallMove():
-    global ball_x, ball_y, ball_dx, ball_dy
+    global ball_x, ball_y, ball_dx, ball_dy, player1_width
     # update ball pos
     ball_x += ball_dx
     ball_y += ball_dy
-    # check collision
+    # check wall collision
     if ball_x <= 0 or ball_x + 30 >= SCREEN_WIDTH:
         ball_dx *= -1
     if ball_y <= 0 or ball_y + 30 >= SCREEN_HEIGHT:
         ball_dy *= -1
+    # check player collision
+    ball_radius = 5
+    if (ball_x + ball_radius >= 0 and
+        ball_x - ball_radius <= 0 + player1_width and
+        ball_y + ball_radius >= player1_y and
+        ball_y - ball_radius <= player1_y + 100):
+        hit_pos = (ball_y - player1_y) / 100
+        ball_dy = (hit_pos - 0.5) * 10
+        ball_dx = abs(ball_dx) * 1.1
     canvas.coords(ball, ball_x, ball_y, ball_x + 30, ball_y + 30)
     window.after(30, BallMove)
 
@@ -44,8 +55,8 @@ canvas = tk.Canvas(window, width=SCREEN_WIDTH, height=SCREEN_HEIGHT, bg='white')
 canvas.pack()
 
 # elements
-player1 = canvas.create_rectangle(0, player1_y, 20, player1_y + 100, fill='black')
-ball = canvas.create_oval(100, 130, 130, 100, fill='black')
+player1 = canvas.create_rectangle(0, player1_y, player1_width, player1_y + 100, fill='black')
+ball = canvas.create_oval(100, ball_size, ball_size, 100, fill='black')
 
 # bind
 window.bind("<Up>", MoveUp)
