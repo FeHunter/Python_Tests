fc8da77db03a0bc350e40604393611030906b990
FeHunter <hunterfe1111@gmail.com>
2025-09-13 04:56:03 -0300

    feat: ball moviment

--- Tenis/tenis.py
+++ Tenis/tenis.py
@@ -1,8 +1,13 @@
 import tkinter as tk
+import random
 
 SCREEN_WIDTH = 500
 SCREEN_HEIGHT = 500
 player1_y = 100
+ball_y = 200
+ball_x = 200
+ball_dx = random.choice([-5, 5])
+ball_dy = random.choice([-5, 5])
 
 def MoveUp(event):
     global player1_y
@@ -16,20 +21,35 @@ def MoveDown(event):
         player1_y += 10
     canvas.coords(player1, 0, player1_y, 20, player1_y + 100)
 
+def BallMove():
+    global ball_x, ball_y, ball_dx, ball_dy
+    # update ball pos
+    ball_x += ball_dx
+    ball_y += ball_dy
+    # check collision
+    if ball_x <= 0 or ball_x + 30 >= SCREEN_WIDTH:
+        ball_dx *= -1
+    if ball_y <= 0 or ball_y + 30 >= SCREEN_HEIGHT:
+        ball_dy *= -1
+    canvas.coords(ball, ball_x, ball_y, ball_x + 30, ball_y + 30)
+    window.after(30, BallMove)
+
 # window settings
 window = tk.Tk()
 window.title("Tenis Game")
 window.geometry("500x500")
 
 # canvas
-canvas = tk.Canvas(window, width=SCREEN_WIDTH, height=SCREEN_HEIGHT, bg='gray')
+canvas = tk.Canvas(window, width=SCREEN_WIDTH, height=SCREEN_HEIGHT, bg='white')
 canvas.pack()
 
-# cria a raquete do player1 uma vez
-player1 = canvas.create_rectangle(0, player1_y, 20, player1_y + 100, fill='blue')
+# elements
+player1 = canvas.create_rectangle(0, player1_y, 20, player1_y + 100, fill='black')
+ball = canvas.create_oval(100, 130, 130, 100, fill='black')
 
-# bind das teclas
+# bind
 window.bind("<Up>", MoveUp)
 window.bind("<Down>", MoveDown)
 
+BallMove()
 window.mainloop()
